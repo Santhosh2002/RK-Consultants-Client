0245923da826df69f1873759c6c2d9337a23d11a
Sai Santhosh <77265502+Santhosh2002@users.noreply.github.com>
2025-02-22 18:59:24 +0530

    file name fix

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,55 +1,63 @@
-import {useEffect,useState} from 'react';
-import FooterComponent from '../components/footer';
-import Navbar from '../components/Navbar';
-import HeroSection from '../components/hero';
-import WhyChooseUs from '../components/WhyChooseUs';
-import WorkDisplaySection from '../components/workDisplaySection';
-import Services from '../components/services';
-import CounterSection from '../components/statsComponent';
-import PropertyListing from '../components/propertyListing';
-import OurClients from '../components/OurClients'
-import RealEstateCTA from '../components/RealEstateCTA';
-import ContactUs from '../components/ContactUsPage';
-import ServicesSection from '../components/ServiceSection';
-import OurJourney from './About/Component/OurJourney';
-import axios from 'axios'
-import OurValues from './About/Component/OurValues';
-import OurAchievements from './About/Component/OurAchievements';
+import { useEffect, useState } from "react";
+import FooterComponent from "../components/footer";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/hero";
+import WhyChooseUs from "../components/WhyChooseUs";
+import WorkDisplaySection from "../components/workDisplaySection";
+import Services from "../components/services";
+import CounterSection from "../components/statsComponent";
+import PropertyListing from "../components/PropertyListing";
+import OurClients from "../components/OurClients";
+import RealEstateCTA from "../components/RealEstateCTA";
+import ContactUs from "../components/ContactUsPage";
+import ServicesSection from "../components/ServiceSection";
+import OurJourney from "./About/Component/OurJourney";
+import axios from "axios";
+import OurValues from "./About/Component/OurValues";
+import OurAchievements from "./About/Component/OurAchievements";
 
 function HomePage() {
-  const visitorUpdate = async()=>{
-    const base= import.meta.env.VITE_BASE_URL ;
-    const url = base + "/api/visitor/"; 
-    await axios.get(url); 
-  }
-  const [general, setGeneral] = useState({});
-  const getGeneralSettings = async()=>{
+  const visitorUpdate = async () => {
     const base = import.meta.env.VITE_BASE_URL;
-    const url = base + "/api/general"; 
-    const response = await axios.get(url); 
+    const url = base + "/api/visitor/";
+    await axios.get(url);
+  };
+  const [general, setGeneral] = useState({});
+  const getGeneralSettings = async () => {
+    const base = import.meta.env.VITE_BASE_URL;
+    const url = base + "/api/general";
+    const response = await axios.get(url);
     setGeneral(response.data.general);
-    return response.data.general; 
-  }
-  useEffect(()=>{
+    return response.data.general;
+  };
+  useEffect(() => {
     visitorUpdate();
-    getGeneralSettings(); 
-  },[]); 
+    getGeneralSettings();
+  }, []);
   return (
     <>
       <Navbar />
-      <HeroSection logo={general.logo}/>
+      <HeroSection logo={general.logo} />
       <ServicesSection />
       {/* <WhyChooseUs about={general.about}/> */}
       <PropertyListing />
       <OurClients />
-      <Services/>
+      <Services />
       <RealEstateCTA />
       {/* <CounterSection/> */}
       {/* <WorkDisplaySection /> */}
       {/* <ContactUs /> */}
-      <FooterComponent address={general.address} phone={general.phone} email={general.email} logo={general.logo} insta={general.instagram} fb={general.facebook}   linkedin={general.linkedin}  />  
+      <FooterComponent
+        address={general.address}
+        phone={general.phone}
+        email={general.email}
+        logo={general.logo}
+        insta={general.instagram}
+        fb={general.facebook}
+        linkedin={general.linkedin}
+      />
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
